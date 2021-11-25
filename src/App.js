5313a21ae8330800ab03a21b68e5bef8b5acaebe
Evin-Ngoa <toneevin07@gmail.com>
2021-11-25 13:01:55 +0300

    Creating Navbar Component and product component bug fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Products from './components/Products/Products'
+
+import { Products, Navbar } from './components'
 
 const App = () => {
     return (
         <div>
-            E-Commerce
+            <Navbar />
             <Products />
         </div>
     )
